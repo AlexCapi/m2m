5776414a44b7fc6c9503ec393a5720f0ba0d6cdd
Yohann Berthon <yohann.berthon@gmail.com>
2014-02-06 11:55:26 +0100

    Fix abstract class agent to class agent

--- src/agent/Extracteur.java
+++ src/agent/Extracteur.java
@@ -1,5 +1,7 @@
 package agent;
 
 public class Extracteur extends Agent{
-
+    public Extracteur(int x, int y) {
+        super(x, y);
+    }
 }
--- src/agent/Transporteur.java
+++ src/agent/Transporteur.java
@@ -1,5 +1,8 @@
 package agent;
 
 public class Transporteur extends Agent{
-
+    public Transporteur(int x, int y) {
+        super(x, y);
+    }
+   
 }
